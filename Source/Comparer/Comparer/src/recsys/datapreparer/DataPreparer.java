55a403ee7e7bbb4686ad547fa5b6d0e41584610c
tuyennta <anhtuyenpro94@gmail.com>
2016-05-24 22:57:28 +0700

    commit mysqlconnection class

--- Source/Comparer/Comparer/src/recsys/datapreparer/DataPreparer.java
+++ Source/Comparer/Comparer/src/recsys/datapreparer/DataPreparer.java
@@ -1,5 +1,11 @@
 package recsys.datapreparer;
 
 public class DataPreparer {
-
+	public static void main(String []args) {
+		MysqlConnection c = new MysqlConnection();
+		if(c.connect()){
+			System.out.print("ok");
+		}else
+			System.out.print("fail");
+	}
 }
