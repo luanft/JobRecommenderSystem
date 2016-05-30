7f269a6a9ed5b9beadf4724c4415b1158b8ee320
Tran Minh Luan <xcrossworker@gmail.com>
2016-05-30 21:57:25 +0700

    add data preparer

--- Source/Comparer/Comparer/src/app/App.java
+++ Source/Comparer/Comparer/src/app/App.java
@@ -1,10 +1,61 @@
 package app;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.FieldInvertState;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.index.Terms;
+import org.apache.lucene.search.CollectionStatistics;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.TermStatistics;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.BytesRef;
+
+import recsys.datapreparer.HybirdRecommenderDataPreparer;
 
 public class App {
 
-	public static void main(String[] args) {		
-				
+	public static void main(String[] args) throws IOException {
+
+		Path index_directory_path = FileSystems.getDefault().getPath("HybridIndex");
+		
+		Directory dir = FSDirectory.open(index_directory_path);
+		
+		DirectoryReader r = DirectoryReader.open(dir);
+		
+		
+		
+		IndexSearcher iSeach = new IndexSearcher(r);
+			    
+		int n = r.maxDoc();
+		for(int i = 0; i < n; i++)
+		{
+			Document d = r.document(i);
+			System.out.println(d.get("JobName"));
+			
+		}
+	    	    
+	    
+		
+//		HybirdRecommenderDataPreparer tesst = new HybirdRecommenderDataPreparer();
+//		try {
+//			tesst.IndexData();
+//		} catch (Exception ex) {
+//			ex.printStackTrace();
+//		}
+		
+		
+		
+		//IndexSearcher iSearch = new IndexSearcher(r)
 	}
 
 }
---
+++ Source/Comparer/Comparer/src/recsys/algorithms/HybirdRecommeder.java
@@ -0,0 +1,5 @@
+package recsys.algorithms;
+
+public class HybirdRecommeder extends  RecommendationAlgorithm{
+
+}
