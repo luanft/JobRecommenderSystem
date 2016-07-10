104f41f1807113d8f114a4951615a3427e5006dd
NGUYEN THANH ANH TUYEN <NGUYEN THANH ANH TUYEN>
2016-07-10 11:02:04 +0700

    no message

---
+++ Source/JobRecSys/JobRecTaskManagement/src/main/java/uit/se/recsys/bean/TaskBean.java
@@ -0,0 +1,82 @@
+package uit.se.recsys.bean;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskBean {
+    int taskId;
+    int userId;
+    @NotNull
+    @NotEmpty
+    String taskName;
+    Timestamp timeCreate;
+    String status;
+    @NotNull
+    @NotEmpty
+    String algorithm;
+    @NotNull
+    @NotEmpty
+    String dataset;
+
+    public int getTaskId() {
+	return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+	this.taskId = taskId;
+    }
+
+    public int getUserId() {
+	return userId;
+    }
+
+    public void setUserId(int userId) {
+	this.userId = userId;
+    }
+
+    public String getTaskName() {
+	return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+	this.taskName = taskName;
+    }
+
+    public String getStatus() {
+	return status;
+    }
+
+    public void setStatus(String status) {
+	this.status = status;
+    }
+
+    public String getAlgorithm() {
+	return algorithm;
+    }
+
+    public void setAlgorithm(String algorithm) {
+	this.algorithm = algorithm;
+    }
+
+    public Timestamp getTimeCreate() {
+        return timeCreate;
+    }
+
+    public void setTimeCreate(Timestamp timeCreate) {
+        this.timeCreate = timeCreate;
+    }
+
+    public String getDataset() {
+	return dataset;
+    }
+
+    public void setDataset(String dataset) {
+	this.dataset = dataset;
+    }
+}
---
+++ Source/JobRecSys/JobRecTaskManagement/src/main/java/uit/se/recsys/utils/DatasetUtil.java
@@ -0,0 +1,115 @@
+package uit.se.recsys.utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import uit.se.recsys.bean.RecommendedItem;
+
+public class DatasetUtil {
+    private static DatasetUtil instance = new DatasetUtil();
+
+    public static DatasetUtil getInstance() {
+	return instance;
+    }
+
+    /**
+     * Get the list dataset in specific location
+     * 
+     * @param path
+     *            {@link String}
+     * @return String[] directories
+     */
+    public String[] getDatasets(String path) {
+	File dir = new File(path);
+	String[] directories = dir.list(new FilenameFilter() {
+	    @Override
+	    public boolean accept(File current, String name) {
+		return new File(current, name).isDirectory();
+	    }
+	});
+	return directories;
+    }
+
+    /**
+     * Get file result in specific location with a file name prefix
+     * 
+     * @param dirPath
+     *            {@link String}
+     * @param fileNamePrefix
+     *            {@link String}
+     * @return fileResult[] {@link File}
+     */
+    public File[] getResultFile(String dirPath, String fileNamePrefix) {
+	File dir = new File(dirPath);
+	return dir.listFiles(new FilenameFilter() {
+
+	    @Override
+	    public boolean accept(File dir, String name) {
+		if (name.startsWith(fileNamePrefix)) {
+		    return true;
+		}
+		return false;
+	    }
+	});
+    }
+
+    /**
+     * This function to read line by line in result file and return a list of RecommendedItem
+     * @param path {@link String}: Full path to result file
+     * @return recommendedItems {@link List<RecommendedItem>}
+     */
+    public List<RecommendedItem> getRecommendedItems(String path) {
+	List<RecommendedItem> recommendedItems = new ArrayList<RecommendedItem>();
+	try {
+	    BufferedReader br = new BufferedReader(new FileReader(path));
+	    String currentLine = "";
+	    while ((currentLine = br.readLine()) != null) {
+		Scanner scanner = new Scanner(currentLine);
+		scanner.useDelimiter("\t");
+		RecommendedItem item = new RecommendedItem();
+		item.setUserId(toInt(scanner.next()));
+		item.setItemId(toInt(scanner.next()));
+		item.setScore(toFloat(scanner.next()));
+		recommendedItems.add(item);
+		scanner.close();
+	    }
+	    br.close();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+	return recommendedItems;
+    }
+
+    /**
+     * Parse an value of string type into integer type
+     * @param data {@link String}
+     * @return valueOfData {@link Integer}
+     */
+    public int toInt(String data) {
+	try {
+	    return Integer.parseInt(data);
+	} catch (Exception e) {
+	    return -1;
+	}
+    }
+
+    /**
+     * Parse an value of string type into float type
+     * @param data {@link String}
+     * @return valueOfData {@link Float}
+     */
+    public float toFloat(String data) {
+	try {
+	    return Float.parseFloat(data);
+	} catch (Exception e) {
+	    return -1.0f;
+	}
+    }
+}
